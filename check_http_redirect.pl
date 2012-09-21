5a3a91bc522499ac8f06e22eba02ea88ec4cceba
Ruslan Kabalin <r.kabalin@lancaster.ac.uk>
2012-09-21 10:37:17 +0100

    Replace tabs with spaces, add vim rule for tabs treatment

--- check_http_redirect.pl
+++ check_http_redirect.pl
@@ -2,8 +2,8 @@
 
 #------------------------------------------------------------------------------
 # Nagios check_http_redirect
-# 	retrieve an http/s url and checks its header for a given redirects
-# 	if the redirect exists and equal to the redirect you entered then exits with OK, otherwise exits with WARNING (if not equal) or CRITICAL ( if doesn't exist)
+#   retrieve an http/s url and checks its header for a given redirects
+#   if the redirect exists and equal to the redirect you entered then exits with OK, otherwise exits with WARNING (if not equal) or CRITICAL ( if doesn't exist)
 #
 # Copyright 2009, Eugene L Kovalenja, http://www.purple.org.ua/
 # Licensed under GPLv2
@@ -28,16 +28,16 @@ use Getopt::Std;
 use LWP::UserAgent;
 
 my $plugin_name = 'Nagios check_http_redirect';
-my $VERSION		= '1.00';
+my $VERSION     = '1.00';
 
 # getopt module config
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
 
 # nagios exit codes
-use constant EXIT_OK		=> 0;
-use constant EXIT_WARNING	=> 1;
-use constant EXIT_CRITICAL	=> 2;
-use constant EXIT_UNKNOWN	=> 3;
+use constant EXIT_OK        => 0;
+use constant EXIT_WARNING   => 1;
+use constant EXIT_CRITICAL  => 2;
+use constant EXIT_UNKNOWN   => 3;
 
 
 # parse cmd opts
@@ -45,9 +45,9 @@ my %opts;
 getopts('vU:R:t:', \%opts);
 $opts{t} = 5 unless (defined $opts{t});
 if (not (defined $opts{U} ) or not (defined $opts{R} )) {
-	print "ERROR: INVALID USAGE\n";
-	HELP_MESSAGE();
-	exit EXIT_CRITICAL;
+    print "ERROR: INVALID USAGE\n";
+    HELP_MESSAGE();
+    exit EXIT_CRITICAL;
 }
 
 my $status = EXIT_OK;
@@ -71,21 +71,21 @@ else
 {
     if ($response->is_redirect)
     {
-	if ( $response->header("Location") =~ $opts{R} )
-	{
-	    print "REDIRECT OK: ", $response->status_line, " ", $response->header("Location"), "\n";
-	    $status = EXIT_OK;
-	}
-	else
-	{
-	    print "REDIRECT WARNING: Location is invalid: ",$response->status_line, " ", $response->header("Location"), "\n";
-	    $status = EXIT_WARNING;
-	}
+        if ( $response->header("Location") =~ $opts{R} )
+        {
+            print "REDIRECT OK: ", $response->status_line, " ", $response->header("Location"), "\n";
+            $status = EXIT_OK;
+        }
+        else
+        {
+            print "REDIRECT WARNING: Location is invalid: ",$response->status_line, " ", $response->header("Location"), "\n";
+            $status = EXIT_WARNING;
+        }
     }
     else
     {
-	print "REDIRECT ERROR: cannot retrieve the url: ", $response->status_line, "\n";
-	$status = EXIT_UNKNOWN;
+        print "REDIRECT ERROR: cannot retrieve the url: ", $response->status_line, "\n";
+        $status = EXIT_UNKNOWN;
     }
 }
 
@@ -93,17 +93,17 @@ exit $status;
 
 sub HELP_MESSAGE
 {
-	print <<EOHELP
- 	Retrieve an http/s url and checks its header for a given redirects.
- 	If the redirect exists and equal to the redirect you entered then exits with OK, otherwise exits with WARNING (if not equal) or CRITICAL ( if doesn't exist)
+    print <<EOHELP
+    Retrieve an http/s url and checks its header for a given redirects.
+    If the redirect exists and equal to the redirect you entered then exits with OK, otherwise exits with WARNING (if not equal) or CRITICAL ( if doesn't exist)
 
-	--help      shows this message
-	--version   shows version information
+    --help      shows this message
+    --version   shows version information
 
-	-U          URL to retrieve (http or https)
-	-R	    URL that must be equal to Header Location Redirect URL
-	-t          Timeout in seconds to wait for the URL to load. If the page fails to load,
-	            $plugin_name will exit with UNKNOWN state (default 60)
+    -U          URL to retrieve (http or https)
+    -R      URL that must be equal to Header Location Redirect URL
+    -t          Timeout in seconds to wait for the URL to load. If the page fails to load,
+                $plugin_name will exit with UNKNOWN state (default 60)
 
 EOHELP
 ;
@@ -112,9 +112,10 @@ EOHELP
 
 sub VERSION_MESSAGE
 {
-	print <<EOVM
+    print <<EOVM
 $plugin_name v. $VERSION
 Copyright 2009, Eugene L Kovalenja, http://www.purple.org.ua/ - Licensed under GPLv2
 EOVM
 ;
 }
+# vi: tabstop=4 shiftwidth=4 expandtab
